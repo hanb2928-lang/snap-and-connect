d6895f707a4a2ca6afe92f77a7b8a8b2e5ea0963
hanb2928-lang <hanb2928@gmail.com>
2026-08-29 01:52:42 +0900

    Adjust height of affiliate marketing sub-tabs

--- hooks/useSubTabBarHeight.ts
+++ hooks/useSubTabBarHeight.ts
@@ -2,5 +2,5 @@ import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export function useSubTabBarHeight(): number {
   const insets = useSafeAreaInsets();
-  return 60 + Math.max(insets.bottom, 0);
+  return 48 + Math.max(insets.bottom, 0);
 }
